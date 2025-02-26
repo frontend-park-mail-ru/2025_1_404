a01d6bd22ea2443900b4d2a4149fea2855426f62
bigronimax <ronimax@bk.ru>
2025-02-26 20:05:31 +0300

    login popup layout

--- public/scripts/components/Login/Login.precompiled.js
+++ public/scripts/components/Login/Login.precompiled.js
@@ -1,3 +1,15 @@
 export default Handlebars.template({"compiler":[8,">= 4.3.0"],"main":function(container,depth0,helpers,partials,data) {
-    return "";
-},"useData":true});
+    var stack1, lookupProperty = container.lookupProperty || function(parent, propertyName) {
+        if (Object.prototype.hasOwnProperty.call(parent, propertyName)) {
+          return parent[propertyName];
+        }
+        return undefined
+    };
+
+  return "<div class=\"overlay\">\r\n    <div class=\"login\">\r\n        <div id=\"loginCloseButton\" class=\"login__close\">\r\n            &times;\r\n        </div>\r\n        <div class=\"login__inner\">\r\n            <div class=\"login__logo\">\r\n                <img class=\"login__logo-img\" src=\"../../../resources/img/logo/logo.svg\" alt=\"\">\r\n            </div>\r\n            <h3 class=\"login__title\">Войти или зарегистрироваться</h3>\r\n            <form class=\"login__form\" method=\"post\" enctype=\"multipart/form-data\" novalidate>\r\n"
+    + ((stack1 = container.invokePartial(lookupProperty(partials,"Input"),depth0,{"name":"Input","hash":{"placeholder":"Email","name":"email","type":"email","id":"emailInput"},"data":data,"indent":"                ","helpers":helpers,"partials":partials,"decorators":container.decorators})) != null ? stack1 : "")
+    + ((stack1 = container.invokePartial(lookupProperty(partials,"Input"),depth0,{"name":"Input","hash":{"placeholder":"Пароль","name":"password","type":"password","id":"passwordInput"},"data":data,"indent":"                ","helpers":helpers,"partials":partials,"decorators":container.decorators})) != null ? stack1 : "")
+    + ((stack1 = container.invokePartial(lookupProperty(partials,"PrimaryButton"),depth0,{"name":"PrimaryButton","hash":{"id":"loginSubmitButton","class":"login__submit","name":"Войти"},"data":data,"indent":"                ","helpers":helpers,"partials":partials,"decorators":container.decorators})) != null ? stack1 : "")
+    + ((stack1 = container.invokePartial(lookupProperty(partials,"LightButton"),depth0,{"name":"LightButton","hash":{"id":"registerHrefButton","class":"register__href","name":"Зарегистрироваться"},"data":data,"indent":"                ","helpers":helpers,"partials":partials,"decorators":container.decorators})) != null ? stack1 : "")
+    + "            </form>\r\n        </div>\r\n    </div>\r\n</div>";
+},"usePartial":true,"useData":true});
--- public/scripts/pages/index/index.js
+++ public/scripts/pages/index/index.js
@@ -10,18 +10,42 @@ export default class IndexPage extends Page {
         pageManager.renderPage('register');
     }
 
+    _loginButtonHandler() {
+        document.querySelector(".login").classList.add('active');
+        document.querySelector(".overlay").classList.add('active');
+    }
+
+    _loginCloseButtonHandler() {
+        document.querySelector(".login").classList.remove('active');
+        document.querySelector(".overlay").classList.remove('active');
+    }
+
     _registerButton = null;
+    _loginButton = null;
+    _loginCloseButton = null;
 
     render(root) {
         root.innerHTML = template();
         this._registerButton = document.getElementById('registerButton');
         this._registerButton.addEventListener('click', () => this._registerButtonHandler());
 
+        this._loginButton = document.getElementById('loginButton');
+        this._loginButton.addEventListener('click', () => this._loginButtonHandler())
+
+        this._loginCloseButton = document.getElementById('loginCloseButton');
+        this._loginCloseButton.addEventListener('click', () => this._loginCloseButtonHandler())
+
     }
 
     destroy() {
         if (this._switchButton) {
             this._switchButton.removeEventListener('click', this._switchButtonHandler);
+        }
+        if (this._loginButton) {
+            this._loginButton.removeEventListener('click', this._loginButtonHandler);
+        }
+        if (this._loginCloseButton) {
+            this._loginCloseButton.removeEventListener('click', this._loginCloseButtonHandler);
         }
     }
 }
