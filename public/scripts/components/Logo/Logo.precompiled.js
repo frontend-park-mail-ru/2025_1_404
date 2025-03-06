b8a4a35c60730223f8ace0ae8a16a164afce15fe
besttwinkever <148713794+besttwinkever@users.noreply.github.com>
2025-03-06 19:02:15 +0300

    Delete public/scripts/components/Logo/Logo.precompiled.js

--- public/scripts/components/Logo/Logo.precompiled.js
+++
@@ -1,3 +0,0 @@
-export default Handlebars.template({"compiler":[8,">= 4.3.0"],"main":function(container,depth0,helpers,partials,data) {
-    return "<div class=\"logo\">\n    <a class=\"logo-href\" href=\"/\">\n        <img class=\"logo-img\" src=\"../../../resources/img/logo/logo.svg\" alt=\"\" >\n    </a>\n    <h3 class=\"logo__title\">квартирум</h3>\n</div>";
-},"useData":true});
