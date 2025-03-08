5d4240f51a579874900f0e3cfc27450b897e180a
MrCreepTon <148713794+besttwinkever@users.noreply.github.com>
2025-03-08 18:57:09 +0300

    eslint

---
+++ eslint.config.js
@@ -0,0 +1,27 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+
+/** @type {import('eslint').Linter.FlatConfig[]} */
+export default [
+  {
+    ignores: ["**/*.precompiled.js", "**/*.min-*.js"],
+  },
+  {
+    rules: {
+      "no-unused-vars": [
+        "error",
+        {
+          varsIgnorePattern: "^_", // Игнорируем переменные, начинающиеся с _
+          argsIgnorePattern: "^_", // Игнорируем параметры, начинающиеся с _
+        },
+      ],
+    },
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        Handlebars: "readonly",
+      },
+    },
+  },
+  pluginJs.configs.recommended,
+];
