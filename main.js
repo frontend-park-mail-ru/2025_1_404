448e5b76632d5ed7467b8eadc7d1e09e0642fea1
besttwinkever <besttwinkever@github.com>
2025-03-27 19:11:12 +0300

    Работа в offline

--- main.js
+++ main.js
@@ -11,6 +11,11 @@ const app = express();
 const DEFAULT_PORT = 8000;
 const SERVER_PORT = process.env.PORT || DEFAULT_PORT;
 
+app.use('/scripts/serviceWorker.js', (req, res, next) => {
+  res.setHeader('Service-Worker-Allowed', '/');
+  next();
+});
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Отдаём index.html для всех маршрутов
